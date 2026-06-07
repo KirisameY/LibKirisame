6c4ed06195ff9fc1e079d140c58828725f9e513f
霧雨烨 <Web_13234@outlook.com>
2026-06-07 20:57:17 +0800

    Task集合的链式WhenAll/WhenAny

---
+++ KirisameY.Relinq/Extensions/TaskEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+
+namespace KirisameY.Relinq.Extensions;
+
+public static class TaskEnumerableExtensions
+{
+    extension(IEnumerable<Task> source)
+    {
+        [PublicAPI]
+        public Task WhenAll() => Task.WhenAll(source);
+
+        [PublicAPI]
+        public Task<Task> WhenAny() => Task.WhenAny(source);
+    }
+
+    extension<T>(IEnumerable<Task<T>> source)
+    {
+        [PublicAPI]
+        public Task<T[]> WhenAll() => Task.WhenAll(source);
+
+        [PublicAPI]
+        public Task<Task<T>> WhenAny() => Task.WhenAny(source);
+    }
+}
