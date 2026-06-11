47ec545e9c6c1e84ffb90ca73c2b7d806ed9e048
霧雨烨 <Web_13234@outlook.com>
2026-06-11 23:44:07 +0800

    NotifiableList

---
+++ KirisameY.NotifiableCollections/INotifiableCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+
+namespace KirisameY.NotifiableCollections;
+
+public interface ICollectionUpdateNotifier<T>
+{
+    public event EventHandler<CollectionItemAddedEventArgs<T>>? ItemAdded;
+    public event EventHandler<CollectionItemRemovedEventArgs<T>>? ItemRemoved;
+    public event EventHandler<CollectionItemReplacedEventArgs<T>>? ItemReplaced;
+}
+
+public interface IListUpdateNotifier<T> : ICollectionUpdateNotifier<T>
+{
+    public new event EventHandler<ListItemAddedEventArgs<T>>? ItemAdded;
+    public new event EventHandler<ListItemRemovedEventArgs<T>>? ItemRemoved;
+    public new event EventHandler<ListItemReplacedEventArgs<T>>? ItemReplaced;
+    public event EventHandler<ListSortedEventArgs<T>>? Sorted;
+
+    event EventHandler<CollectionItemRemovedEventArgs<T>>? ICollectionUpdateNotifier<T>.ItemRemoved
+    {
+        add => ItemRemoved += value;
+        remove => ItemRemoved -= value;
+    }
+    event EventHandler<CollectionItemReplacedEventArgs<T>>? ICollectionUpdateNotifier<T>.ItemReplaced
+    {
+        add => ItemReplaced += value;
+        remove => ItemReplaced -= value;
+    }
+    event EventHandler<CollectionItemAddedEventArgs<T>>? ICollectionUpdateNotifier<T>.ItemAdded
+    {
+        add => ItemAdded += value;
+        remove => ItemAdded -= value;
+    }
+}
+
+public interface IReadonlyNotifiableCollection<T> : IReadOnlyCollection<T>, ICollectionUpdateNotifier<T>;
+
+public interface INotifiableCollection<T> : ICollection<T>, IReadonlyNotifiableCollection<T>
+{
+    public new int Count { get; }
+
+    int ICollection<T>.Count => Count;
+    int IReadOnlyCollection<T>.Count => Count;
+}
+
+public interface IReadonlyNotifiableList<T> : IReadOnlyList<T>, IListUpdateNotifier<T>;
+
+public interface INotifiableList<T> : IList<T>, IReadonlyNotifiableList<T>
+{
+    public new int Count { get; }
+    public new T this[int index] { get; set; }
+
+
+    T IList<T>.this[int index]
+    {
+        get => this[index];
+        set => this[index] = value;
+    }
+    T IReadOnlyList<T>.this[int index] => this[index];
+
+    int ICollection<T>.Count => Count;
+    int IReadOnlyCollection<T>.Count => Count;
+}
---
+++ KirisameY.NotifiableCollections/NotifiableList.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+using KirisameY.Relinq.Extensions;
+
+namespace KirisameY.NotifiableCollections;
+
+public class NotifiableList<T> : INotifiableList<T>
+{
+    private readonly List<T> _innerList = [];
+    private IReadOnlyList<T> Readonly => field ??= _innerList.AsReadOnly();
+
+
+    #region Reading
+
+    public IEnumerator<T> GetEnumerator() => _innerList.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_innerList).GetEnumerator();
+
+    public bool Contains(T item) => _innerList.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex) => _innerList.CopyTo(array, arrayIndex);
+
+    public int Count => _innerList.Count;
+
+    public bool IsReadOnly => false;
+
+    public int IndexOf(T item) => _innerList.IndexOf(item);
+
+    #endregion
+
+
+    #region Converting
+
+    // AsReadOnly
+
+    #endregion
+
+
+    public T this[int index]
+    {
+        get => _innerList[index];
+        set
+        {
+            var oldValue = _innerList[index];
+            _innerList[index] = value;
+            ItemReplaced?.Invoke(this, new([oldValue], [value], [index]));
+        }
+    }
+
+    public void Add(T item)
+    {
+        _innerList.Add(item);
+        ItemAdded?.Invoke(this, new([item], _innerList.Count - 1));
+    }
+
+    public void AddRange(ICollection<T> items)
+    {
+        var fromIndex = _innerList.Count;
+        _innerList.AddRange(items);
+        ItemAdded?.Invoke(this, new([..items], fromIndex));
+    }
+
+    public void AddRange(IEnumerable<T> items) => AddRange([..items]);
+
+    public void Insert(int index, T item)
+    {
+        _innerList.Insert(index, item);
+        ItemAdded?.Invoke(this, new([item], index));
+    }
+
+    public void InsertRange(int index, ICollection<T> items)
+    {
+        _innerList.InsertRange(index, items);
+        ItemAdded?.Invoke(this, new([..items], index));
+    }
+
+    public void InsertRange(int index, IEnumerable<T> items) => InsertRange(index, [..items]);
+
+    public void Clear()
+    {
+        var before = _innerList.ToImmutableArray();
+        _innerList.Clear();
+        ItemRemoved?.Invoke(this, new(before, [..Enumerable.Range(0, before.Length)], true));
+    }
+
+    public bool Remove(T item)
+    {
+        var index = _innerList.IndexOf(item);
+        if (index < 0) return false;
+        _innerList.RemoveAt(index);
+        ItemRemoved?.Invoke(this, new([item], [index], _innerList.Count == 0));
+        return true;
+    }
+
+    public void RemoveAt(int index)
+    {
+        var item = _innerList[index];
+        _innerList.RemoveAt(index);
+        ItemRemoved?.Invoke(this, new([item], [index], _innerList.Count == 0));
+    }
+
+    public void RemoveRange(int index, int count)
+    {
+        var removed = _innerList[index..(index + count)];
+        var removedIndexes = Enumerable.Range(index, count).ToImmutableList();
+        _innerList.RemoveRange(index, count);
+        ItemRemoved?.Invoke(this, new(removed, removedIndexes, _innerList.Count == 0));
+    }
+
+    public void RemoveAll(Predicate<T> predicate)
+    {
+        var indexes = _innerList.Select((item, index) => predicate.Invoke(item) ? index : -1)
+                                .Where(i => i >= 0).ToImmutableList();
+        var items = indexes.Reverse().Select(i =>
+        {
+            var item = _innerList[i];
+            _innerList.RemoveAt(i);
+            return item;
+        }).Reverse().ToImmutableList();
+        ItemRemoved?.Invoke(this, new(items, indexes, _innerList.Count == 0));
+    }
+
+    public void Sort()
+    {
+        _innerList.Sort();
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+    public void Sort(Comparison<T> comparison)
+    {
+        _innerList.Sort(comparison);
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+    public void Sort(IComparer<T> comparer)
+    {
+        _innerList.Sort(comparer);
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+    public void Sort(int index, int count, IComparer<T> comparer)
+    {
+        _innerList.Sort(index, count, comparer);
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+    public void Reverse()
+    {
+        _innerList.Reverse();
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+    public void Reverse(int index, int count)
+    {
+        _innerList.Reverse(index, count);
+        Sorted?.Invoke(this, new(Readonly));
+    }
+
+
+    public event EventHandler<ListItemAddedEventArgs<T>>? ItemAdded;
+    public event EventHandler<ListItemRemovedEventArgs<T>>? ItemRemoved;
+    public event EventHandler<ListItemReplacedEventArgs<T>>? ItemReplaced;
+    public event EventHandler<ListSortedEventArgs<T>>? Sorted;
+}
