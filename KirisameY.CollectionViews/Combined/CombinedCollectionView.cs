3aa1484440ad2e61961457b30e89ff93ff409bcf
霧雨烨 <Web_13234@outlook.com>
2026-09-17 04:42:25 +0800

    CombinedCollectionViews

---
+++ KirisameY.CollectionViews/Combined/CombinedCollectionView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+using KirisameY.Relinq.Extensions;
+
+namespace KirisameY.CollectionViews.Combined;
+
+public class CombinedCollectionView<T>(IReadOnlyCollection<IReadOnlyCollection<T>> collections) : IReadOnlyCollection<T>
+{
+    public static CombinedCollectionView<T> Create(IReadOnlyCollection<IReadOnlyCollection<T>> collections) => new(collections);
+    public static CombinedCollectionView<T> CreateFrozen(IReadOnlyCollection<IReadOnlyCollection<T>> collections) => new([..collections]);
+
+    public IEnumerator<T> GetEnumerator() => collections.Flatten().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => collections.Sum(c => c.Count);
+}
---
+++ KirisameY.CollectionViews/Combined/CombinedListView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+using KirisameY.Relinq.Extensions;
+
+namespace KirisameY.CollectionViews.Combined;
+
+public class CombinedListView<T>(IReadOnlyCollection<IReadOnlyList<T>> lists) : IReadOnlyList<T>
+{
+    public static CombinedListView<T> Create(IReadOnlyCollection<IReadOnlyList<T>> lists) => new(lists);
+    public static CombinedListView<T> CreateFrozen(IEnumerable<IReadOnlyList<T>> lists) => new([..lists]);
+
+
+    public IEnumerator<T> GetEnumerator() => lists.Flatten().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => lists.Sum(l => l.Count);
+    public T this[int index]
+    {
+        get
+        {
+            var i = index;
+            foreach (var list in lists)
+            {
+                if (i < list.Count) return list[i];
+                i -= list.Count;
+            }
+
+            throw new IndexOutOfRangeException($"Index is {index} but {nameof(CombinedListView<>)} length is {Count}");
+        }
+    }
+}
