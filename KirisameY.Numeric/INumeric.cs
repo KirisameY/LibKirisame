f3da8535973c0c100ececde410addd5df6c9c14f
霧雨烨 <Web_13234@outlook.com>
2026-09-17 04:14:46 +0800

    Numeric 接口定义

---
+++ KirisameY.Numeric/INumeric.cs
@@ -0,0 +1,24 @@
+﻿namespace KirisameY.Numeric;
+
+public interface INumeric<out T>
+{
+    T BaseValue { get; }
+    T Value { get; }
+}
+
+public interface IBaseEditableNumeric<T> : INumeric<T>
+{
+    new T BaseValue { get; set; }
+}
+
+public interface IModifierEditableNumeric<out T, TOrder> : INumeric<T>
+    where TOrder : Enum
+{
+    IReadOnlyCollection<INumericModifier<TOrder>> Modifiers { get; }
+
+    void AddModifier(INumericModifier<TOrder> modifier);
+    bool RemoveModifier(INumericModifier<TOrder> modifier);
+}
+
+public interface IEditableNumeric<T, TOrder> : IBaseEditableNumeric<T>, IModifierEditableNumeric<T, TOrder>
+    where TOrder : Enum;
---
+++ KirisameY.Numeric/INumericModifier.cs
@@ -0,0 +1,11 @@
+﻿namespace KirisameY.Numeric;
+
+public interface INumericModifier
+{
+    void ModifyValue(ref double value);
+}
+
+public interface INumericModifier<out TOrder> : INumericModifier where TOrder : Enum
+{
+    TOrder Order { get; }
+}
